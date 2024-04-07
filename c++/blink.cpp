136c6ec3da88bc09642a227c29006bbe0b3d725c
Karthik.Selvaraj <karthik.selvaraj@nordson.com>
2024-04-07 16:46:09 +0530

    temp and humidity added

--- c++/blink.cpp
+++ c++/blink.cpp
@@ -15,16 +15,76 @@ using namespace httplib;
 
 #define MAXTIMINGS 85
 #define DHTPIN 7
-int dht11_dat[5] = {0,0,0,0,0};
+int dht11_dat[5] = {0, 0, 0, 0, 0};
 
 struct Data {
     time_t timestamp;
     bool state;
+    float temperature; // Added temperature field
+    float humidity;    // Added humidity field
 };
 
 deque<Data> circularBuffer;
 const int bufferCapacity = 3600;
 
+void read_dht11_dat() {
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    float f;
+
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+    pinMode(DHTPIN, OUTPUT);
+    digitalWrite(DHTPIN, LOW);
+    delay(18);
+    digitalWrite(DHTPIN, HIGH);
+    delayMicroseconds(40);
+    pinMode(DHTPIN, INPUT);
+
+    for (i = 0; i < MAXTIMINGS; i++) {
+        counter = 0;
+        while (digitalRead(DHTPIN) == laststate) {
+            counter++;
+            delayMicroseconds(1);
+            if (counter == 255) {
+                break;
+            }
+        }
+        laststate = digitalRead(DHTPIN);
+
+        if (counter == 255)
+            break;
+
+        if ((i >= 4) && (i % 2 == 0)) {
+            dht11_dat[j / 8] <<= 1;
+            if (counter > 16)
+                dht11_dat[j / 8] |= 1;
+            j++;
+        }
+    }
+
+    if ((j >= 40) &&
+        (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF))) {
+        float humidity = (float)dht11_dat[0] + (float)dht11_dat[1] / 10.0;
+        float temperature = (float)dht11_dat[2] + (float)dht11_dat[3] / 10.0;
+        printf("Humidity = %.1f %% Temperature = %.1f C (%.1f F)\n", humidity, temperature, temperature * 9 / 5 + 32);
+
+        // Add temperature and humidity readings to circular buffer
+        Data newData;
+        newData.timestamp = time(nullptr);
+        newData.temperature = temperature;
+        newData.humidity = humidity;
+        circularBuffer.push_back(newData);
+
+        // Remove oldest data if buffer exceeds capacity
+        if (circularBuffer.size() > bufferCapacity) {
+            circularBuffer.pop_front();
+        }
+    } else {
+        printf("Data not good, skip\n");
+    }
+}
 
 void updateState() {
     bool state = digitalRead(1);
@@ -51,25 +111,26 @@ void updateState() {
 
 void serverThread() {
     Server server;
-    
+
     server.Get("/latest_state", [](const Request& req, Response& res) {
-    if (circularBuffer.empty()) {
-        res.status = 404;
-        res.set_content("No data available", "text/plain");
-        cout << "No data available for latest_state endpoint" << endl;
-        return;
-    }
+        if (circularBuffer.empty()) {
+            res.status = 404;
+            res.set_content("No data available", "text/plain");
+            cout << "No data available for latest_state endpoint" << endl;
+            return;
+        }
 
-    const Data& latestData = circularBuffer.back();
-    string response = "{";
-    response += "\"state\": " + to_string(latestData.state) + ",";
-    response += "\"timestamp\": " + to_string(latestData.timestamp);
-    response += "}";
+        const Data& latestData = circularBuffer.back();
+        string response = "{";
+        response += "\"state\": " + to_string(latestData.state) + ",";
+        response += "\"temperature\": " + to_string(latestData.temperature) + ",";
+        response += "\"humidity\": " + to_string(latestData.humidity) + ",";
+        response += "\"timestamp\": " + to_string(latestData.timestamp);
+        response += "}";
 
-    res.set_content(response, "application/json");
-    cout << "Request received for latest_state endpoint" << endl;
-});
-    
+        res.set_content(response, "application/json");
+        cout << "Request received for latest_state endpoint" << endl;
+    });
 
     server.Get("/all_data", [](const Request& req, Response& res) {
         if (circularBuffer.empty()) {
@@ -82,7 +143,9 @@ void serverThread() {
         string response = "[";
         for (const auto& entry : circularBuffer) {
             response += "{ \"timestamp\": " + to_string(entry.timestamp) +
-                        ", \"state\": " + to_string(entry.state) + " },";
+                        ", \"state\": " + to_string(entry.state) +
+                        ", \"temperature\": " + to_string(entry.temperature) +
+                        ", \"humidity\": " + to_string(entry.humidity) + " },";
         }
         response.pop_back();
         response += "]";
@@ -106,7 +169,8 @@ int main() {
 
     // Main program loop
     while (1) {
-        updateState();
+        read_dht11_dat(); // Read DHT11 data
+        updateState();    // Update state
         delay(1000);
     }
 
