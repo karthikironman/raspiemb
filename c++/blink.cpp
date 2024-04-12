7fb4e0306d73fbdec872865261d84681ac764b70
Karthik.Selvaraj <karthik.selvaraj@nordson.com>
2024-04-12 12:39:30 +0530

    removed unused variale

--- c++/blink.cpp
+++ c++/blink.cpp
@@ -33,7 +33,6 @@ void read_dht11_dat()
     uint8_t laststate = HIGH;
     uint8_t counter = 0;
     uint8_t j = 0, i;
-    float f;
 
     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
